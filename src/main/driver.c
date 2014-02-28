23ae61919eff092c31eeefbf439a637063433b95
Josh DeWitt <jdewitt@unomaha.edu>
2014-02-27 22:38:02 -0600

    Increased the communication speed.

--- src/main/driver.c
+++ src/main/driver.c
@@ -59,7 +59,7 @@ static void setupSpiPins( void ) {
     bcm2835_spi_begin();
     bcm2835_spi_setBitOrder( BCM2835_SPI_BIT_ORDER_MSBFIRST );
     bcm2835_spi_setDataMode( BCM2835_SPI_MODE0 );
-    bcm2835_spi_setClockDivider( BCM2835_SPI_CLOCK_DIVIDER_65536 );
+    bcm2835_spi_setClockDivider( BCM2835_SPI_CLOCK_DIVIDER_256 );
     bcm2835_spi_chipSelect( BCM2835_SPI_CS0 );
     bcm2835_spi_setChipSelectPolarity( BCM2835_SPI_CS0, LOW );
 }
