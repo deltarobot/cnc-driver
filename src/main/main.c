ffe62555a760c79a43805910865307eb760982c5
Josh DeWitt <chessami92@gmail.com>
2014-03-11 22:22:56 -0500

    Made stdin pull character-by-character.

--- src/main/main.c
+++ src/main/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
 #include "bootload.h"
 #include "driver.h"
 
@@ -8,6 +9,8 @@ typedef int ( *LineHandler )( char *line );
 
 #define BOOTLOAD_ON 0xFE
 #define BOOTLOAD_OFF 0xFF
+
+static int setCharByChar( void );
 
 int main( void ) {
     char *line = NULL;
@@ -17,6 +20,8 @@ int main( void ) {
     if( !gpioInit() ) {
         exit( EXIT_FAILURE );
     }
+
+    setCharByChar();
 
     while( getline( &line, &size, stdin ) != -1 ) {
         switch( ( unsigned char )line[0] ) {
@@ -40,5 +45,21 @@ int main( void ) {
 
     free( line );
     exit( EXIT_SUCCESS );
+}
+
+static int setCharByChar( void ) {
+    struct termios tio, newTio;
+
+    if( tcgetattr( 0, &tio ) == -1 ) {
+        return 0;
+    }
+
+    newTio = tio;
+    newTio.c_lflag &= ~ICANON; /* Non-canonical mode */
+
+    if( tcsetattr( 0, TCSANOW, &tio ) == -1 ) {
+        return 0;
+    }
+    return 1;
 }
 
